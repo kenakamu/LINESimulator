38364cb15899b0e61e7125cb8f0087fd699b6cc7
KENICHIRO NAKAMURA <kenakamu@KENICHIROs-MacBook-Air.local>
2018-07-27 10:32:04 -0700

    add mac package

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@
     }
   });
 
-  (function(){
+  (function () {
     if (require('electron-squirrel-startup')) return;
   })();
   // Express part
@@ -329,6 +329,19 @@
             }
           }
         ]
+      },
+      {
+        label: "Edit",
+        submenu: [
+          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+          { type: "separator" },
+          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]
+
       }
     ]);
 
